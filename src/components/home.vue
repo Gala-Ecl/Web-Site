2b69c79e8fb0a7ec583f06782e85862d1e916911
WackbarFoucart <foucart.flavien@gmail.com>
2020-05-21 18:54:49 +0200

    Première avancée de Wack

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,9 +1,31 @@
 <template>
-  <section class="container">
-    <h1>Bienvenue sur le site du gala</h1>
-    <br/>
-    <a href="/party">La soirée</a>
-    <br/>
-    <a href="/login">Se connecter</a>
+  <section>
+    <section class="section-logo-gala">
+    </section>
+    <section class="section-bg-image-1">
+    </section>
+    <section class="countdown">
+    [200 jours 15 heures 20 min 18 sec]
+    </section>
+    <section class="standard-content">
+      <h1>Bienvenue sur le site du gala</h1>
+      <br/>
+      Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna. Nunc viverra imperdiet enim. Fusce est. Vivamus a tellus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin pharetra nonummy pede. Mauris et orci.
+    </section>
+    <section class="section-bg-image-2">
+    </section>
+    <section class="section-logo-edition">
+    </section>
+    <section class="section-bg-image-3">
+    </section>
+    <section>
+      <h1>Bienvenue sur le site du gala</h1>
+      <br/>
+      <a class="main-button" href="/party">La soirée</a>
+      <br/>
+      <br/>
+      <br/>
+      <a class="main-button" href="/login">Se connecter</a>
+    </section>
   </section>
 </template>
