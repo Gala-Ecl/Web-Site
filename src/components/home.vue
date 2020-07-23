e2a93c1ba0b99f70ce950833ef14b42e54487c78
WackbarFoucart <foucart.flavien@gmail.com>
2020-07-23 13:14:32 +0200

    Premier jet - Page de connexion

--- src/components/home.vue
+++ src/components/home.vue
@@ -13,22 +13,67 @@
     </section>
     <section class="section-bg-image-1">
       <div class="container-gallery">
-        <div class="image-box">
+        <div class="image-2-1-box">
           <img src="../../Bonif.jpeg">
-          <div class="text-on-image">
+          <div class="text-on-image" id="color">
           <h1>Titre de l'image</h1>
+          <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna. Nunc viverra imperdiet enim. Fusce est. Vivamus a tellus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin pharetra nonummy pede. Mauris et orci.</p>
+          </div>
+        </div>
+        <div class="image-1-1-box">
+          <img src="../../Bonif.jpeg">
+          <div class="text-on-image" id="color">
+          <h1>Titre de l'image</h1>
+          <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna. Nunc viverra imperdiet enim. Fusce est. Vivamus a tellus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin pharetra nonummy pede. Mauris et orci.</p>
+          </div>
+        </div>
+        <div class="image-1-1-box">
+          <img src="../../Bonif.jpeg">
+          <div class="text-on-image" id="color">
+          <h1>Titre de l'image</h1>
+          <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna. Nunc viverra imperdiet enim. Fusce est. Vivamus a tellus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin pharetra nonummy pede. Mauris et orci.</p>
+          </div>
+        </div>
+        <div class="image-1-1-box">
+          <img src="../../Bonif.jpeg">
+          <div class="text-on-image" id="color">
+          <h1>Titre de l'image</h1>
+          <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna. Nunc viverra imperdiet enim. Fusce est. Vivamus a tellus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin pharetra nonummy pede. Mauris et orci.</p>
+          </div>
+        </div>
+        <div class="image-4-3-box">
+          <img src="../../logo_gala_2019_infos.jpg">
+          <div class="text-on-image" id="color">
+          <h1>Titre de l'image</h1>
+          <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna. Nunc viverra imperdiet enim. Fusce est. Vivamus a tellus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin pharetra nonummy pede. Mauris et orci.</p>
+          </div>
+        </div>
+        <div class="image-16-9-box">
+          <img src="../../logo_gala_2019_infos.jpg">
+          <div class="text-on-image" id="color">
+          <h1>Titre de l'image</h1>
+          <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna. Nunc viverra imperdiet enim. Fusce est. Vivamus a tellus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin pharetra nonummy pede. Mauris et orci.</p>
+          </div>
+        </div>
+        <div class="image-A4-box">
+          <img src="../../logo_gala_2019_infos.jpg">
+          <div class="text-on-image" id="color">
+          <h1>Titre de l'image</h1>
+          <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna. Nunc viverra imperdiet enim. Fusce est. Vivamus a tellus.</p>
           </div>
         </div>
       </div>
     </section>
-    <section class="section-countdown">
-      <h2><span>Compte à Rebours</span> avant le Gala 2020 </h2>
-      <div class="countdown">
-        <div id="day">{{days}}</div>
-        <div id="hour">{{hours}}</div>
-        <div id="minute">{{minutes}}</div>
-        <div id="second">{{seconds}}</div>
-      </div>
+    <section class="section-countdown-border">
+      <section class="section-countdown">
+        <h2><span>Compte à Rebours</span> avant le Gala 2020 </h2>
+        <div class="countdown">
+          <div id="day">{{days}}</div>
+          <div id="hour">{{hours}}</div>
+          <div id="minute">{{minutes}}</div>
+          <div id="second">{{seconds}}</div>
+        </div>
+      </section>
     </section>
     <section class="section-bg-image-1">
     </section>
@@ -46,6 +91,40 @@
     </section>
     <section class="section-bg-image-2"></section>
     <section class="section-logo-edition"></section>
+    <section class="section-bg-image-1">
+      <div class="container-gallery">
+        <div class="image-2-1-box" id="zoom">
+          <img src="../../Bonif.jpeg">
+          <div class="text-on-image" id="zoom">
+          <h1>Titre de l'image</h1>
+          </div>
+        </div>
+        <div class="image-1-1-box" id="zoom">
+          <img src="../../Bonif.jpeg">
+          <div class="text-on-image" id="zoom">
+          <h1>Titre de l'image</h1>
+          </div>
+        </div>
+        <div class="image-4-3-box" id="zoom">
+          <img src="../../logo_gala_2019_infos.jpg">
+          <div class="text-on-image" id="zoom">
+          <h1>Titre de l'image</h1>
+          </div>
+        </div>
+        <div class="image-16-9-box" id="zoom">
+          <img src="../../logo_gala_2019_infos.jpg">
+          <div class="text-on-image" id="zoom">
+          <h1>Titre de l'image</h1>
+          </div>
+        </div>
+        <div class="image-A4-box" id="zoom">
+          <img src="../../logo_gala_2019_infos.jpg">
+          <div class="text-on-image" id="zoom">
+          <h1>Titre de l'image</h1>
+          </div>
+        </div>
+      </div>
+    </section>
     <section class="section-bg-image-3"></section>
     <section>
       <h1>Bienvenue sur le site du gala</h1>
@@ -63,7 +142,7 @@
 export default {
   data() {
     return {
-      beginningDatetime: new Date("2020-11-28T20:00:00"),
+      beginningDatetime: new Date("2020-11-28T18:00:00"),
       days : undefined,
       hours : undefined,
       minutes : undefined,
--- src/components/login.vue
+++ src/components/login.vue
@@ -1,15 +1,43 @@
 <template>
-  <section class="container">
-    <label>Email :</label>
-    <br />
-    <input v-model="email" placeholder="galaEclyps" />
-    <br />
-    <label>Mot de passe :</label>
-    <br />
-    <input type="password" v-model="password" placeholder="galaEclyps" />
-    <br />
+  <section>
+    <section class="section-bandeau">
+    </section>
+  <section class="container-form">
+    <h1> Veuillez vous connecter </h1>
+    <p> Vous avez déjà un compte Gala Eclyps </p>
+    <div class="field">
+      <br />
+      <input v-model="email" id="email" name="email" required/>
+      <span>Email</span>
+      <br />
+    </div>
+    <div class="field">
+      <input type="password" v-model="password" required/>
+      <span>Mot de passe</span>
+      <br />
+    </div>
     <button @click="connection">Se connecter</button>
+    <br />
   </section>
+  <section class="container-form">
+    <p> Vous n'avez pas encore de compte ? </p>
+    <button @click="connection">S'inscrire <i class="mdi mdi-chevron-down" /></button>
+  </section>
+  <section class="container-form">
+      <h1> Veuillez créer votre compte </h1>
+    <div class="field">
+      <input v-model="email" id="email" name="email" required/>
+      <span>Email</span>
+      <br />
+    </div>
+    <div class="field">
+      <input type="password" v-model="password" required/>
+      <span>Mot de passe</span>
+      <br />
+    </div>
+    <button @click="connection">Se connecter</button>    
+  </section>
+</section>
 </template>
 <script>
 import axios from "axios";
