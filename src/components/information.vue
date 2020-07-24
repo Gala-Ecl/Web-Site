08384f3c39b0d9a158bbf51d76b3a5be2a5a34ae
WackbarFoucart <foucart.flavien@gmail.com>
2020-07-24 19:13:50 +0200

    Création d'un slider

---
+++ src/components/information.vue
@@ -0,0 +1,41 @@
+ <template>
+  <section>
+    <section class="section-bandeau"> 
+    </section>
+    <section> 
+      <div class="slider">
+        <input type="radio" id="show_slide1" name="slider_commands">
+        <input type="radio" id="show_slide2" name="slider_commands">
+        <input type="radio" id="show_slide3" name="slider_commands">
+        <div class="slides">
+          <figure id="slide1">
+              <img src="../../diplomes.png" alt="La journée">
+              <figcaption><h1>La Journée</h1></figcaption>
+          </figure>
+          <figure id="slide2">
+              <img src="../../diner.jpg" alt="Le dîner">
+              <figcaption><h1>Le Dîner</h1></figcaption>
+          </figure>
+          <figure id="slide3">
+              <img src="../../barnums.png" alt="La Soirée">
+              <figcaption><h1>La Soirée</h1></figcaption>
+          </figure>
+        </div>
+        <nav>
+          <ul class="dots_commands">
+            <li><label for="show_slide1">Slide 1</label></li>
+            <li><label for="show_slide2">Slide 2</label></li>
+            <li><label for="show_slide3">Slide 3</label></li>
+          </ul>
+        </nav>
+      </div>
+    </section>
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
