2ed2a1c53f7db976e740e4c1e2f6ac4e2ef27083
WackbarFoucart <foucart.flavien@gmail.com>
2020-07-25 14:57:29 +0200

    Responsive design for photobanner and slider

--- src/components/graduation.vue
+++ src/components/graduation.vue
@@ -1,5 +1,19 @@
  <template>
-  <section class="container">Hello</section>
+  <section>
+    <section class="section-bandeau">
+    </section>
+    <section class="section-banner">
+      <div class="photobanner">
+        <img class="first" src="../../Info_Defilante_1.png">
+        <img src="../../Info_Defilante_2.png">
+        <img src="../../Info_Defilante_3.png">
+        <img src="../../Info_Defilante_4.png">
+        <img src="../../Info_Defilante_1.png"> <!-- Remettre les 4 premieres pour effet de continuite -->
+        <img src="../../Info_Defilante_2.png">
+        <img src="../../Info_Defilante_3.png">
+      </div>
+    </section>
+  </section>
 </template>
 
 <script>
--- src/components/information.vue
+++ src/components/information.vue
@@ -2,6 +2,17 @@
   <section>
     <section class="section-bandeau"> 
     </section>
+    <section class="section-banner">
+      <div class="photobanner">
+        <img class="first" src="../../Info_Defilante_1.png">
+        <img src="../../Info_Defilante_2.png">
+        <img src="../../Info_Defilante_3.png">
+        <img src="../../Info_Defilante_4.png">
+        <img src="../../Info_Defilante_1.png"> <!-- Remettre les 4 premieres pour effet de continuite -->
+        <img src="../../Info_Defilante_2.png">
+        <img src="../../Info_Defilante_3.png">
+      </div>
+    </section>
     <section> 
       <div class="slider">
         <input type="radio" id="show_slide1" name="slider_commands">
