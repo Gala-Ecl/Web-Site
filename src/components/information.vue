ab9cf5c3e8795d591b883cae47c1242b9f880328
TPennamen <thomas.pennamen@gmail.com>
2020-12-29 21:05:18 +0100

    Added selection image

--- src/components/information.vue
+++ src/components/information.vue
@@ -4,14 +4,23 @@
     </section>
     <section class="section-banner">
       <div class="photobanner">
-        <a  href="/party"><img class="first" src="../../Info_Defilante_1.png"></a>
-        <a  href="/party"><img src="../../Info_Defilante_2.png"></a>
-        <a  href="/party"><img src="../../Info_Defilante_3.png"></a>
-        <a  href="/party"><img src="../../Info_Defilante_4.png"></a>
-        <a  href="/party"><img src="../../Info_Defilante_1.png"></a> <!-- Remettre les 4 premieres pour effet de continuite -->
-        <a  href="/party"><img src="../../Info_Defilante_2.png"></a>
-        <a  href="/party"><img src="../../Info_Defilante_3.png"></a>
+        <a  @click="imageChosenSource=1"><img class="first" src="../../Info_Defilante_1.png"></a>
+        <a  @click="imageChosenSource=2"><img src="../../Info_Defilante_2.png"></a>
+        <a  @click="imageChosenSource=3"><img src="../../Info_Defilante_3.png"></a>
+        <a  @click="imageChosenSource=4"><img src="../../Info_Defilante_4.png"></a>
+        <a  @click="imageChosenSource=5"><img src="../../Info_Defilante_1.png"></a> <!-- Remettre les 4 premieres pour effet de continuite -->
+        <a  @click="imageChosenSource=6"><img src="../../Info_Defilante_2.png"></a>
+        <a  @click="imageChosenSource=7"><img src="../../Info_Defilante_3.png"></a>
       </div>
+    </section>
+    <section class="section-bg-image-2">
+      <img v-if="imageChosenSource==1" src="../../Info_Defilante_1.png">
+      <img v-if="imageChosenSource==2" src="../../Info_Defilante_2.png">
+      <img v-if="imageChosenSource==3" src="../../Info_Defilante_3.png">
+      <img v-if="imageChosenSource==4" src="../../Info_Defilante_4.png">
+      <img v-if="imageChosenSource==5" src="../../Info_Defilante_1.png"> <!-- Remettre les 4 premieres pour effet de continuite -->
+      <img v-if="imageChosenSource==6" src="../../Info_Defilante_2.png">
+      <img v-if="imageChosenSource==7" src="../../Info_Defilante_3.png">
     </section>
     <section class="section-bg-image-2">
       <div class="tickets">
@@ -37,7 +46,14 @@
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      imageChosenSource : 1
+    }
+  },
+
+};
 </script>
 
 <style>
